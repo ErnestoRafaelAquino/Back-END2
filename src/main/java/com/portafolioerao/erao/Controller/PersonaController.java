c0fd0e135d7d5ce893c335e68499fd6167759eb9
ErnestoRafaelAquino <ernestorafaelaquino@outlook.es>
2022-08-17 20:04:12 -0300

    creacion_usuarios_rol

--- src/main/java/com/portafolioerao/erao/Controller/PersonaController.java
+++ src/main/java/com/portafolioerao/erao/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portafolioerao.erao.Entity.Persona;
 import com.portafolioerao.erao.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,18 +29,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "Creado con exito";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "Borrado con exito";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
