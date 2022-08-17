9dc6c39df2c00cc9a5579ea759dba63fd5f1e193
ErnestoRafaelAquino <ernestorafaelaquino@outlook.es>
2022-08-17 16:09:32 -0300

    Initial commit

---
+++ src/main/java/com/portafolioerao/erao/EraoApplication.java
@@ -0,0 +1,13 @@
+package com.portafolioerao.erao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EraoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portafolioerao/erao/EraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolioerao.erao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
