058175fed4d2c7f21e9af03f310740910a231e14
ErnestoRafaelAquino <ernestorafaelaquino@outlook.es>
2022-08-19 17:24:49 -0300

    no_registrado
    cambio desconocido

--- src/main/java/com/portafolioerao/erao/Controller/PersonaController.java
+++ src/main/java/com/portafolioerao/erao/Controller/PersonaController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portafolioerao.erao.Controller;
 
 import com.portafolioerao.erao.Entity.Persona;
